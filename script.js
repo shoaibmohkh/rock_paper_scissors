9f2577c5ad6b11be4b88852cccff69637b8dde62
Shoaib Mohkh <shoaibmohkh@gmail.com>
2025-07-16 12:30:04 +0300

    add buttons

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ let user_score = 0;
 let cpu_score = 0;
 let round = 0;
 
-function user_Input()
+function user_Input(user)
 {
-    let user = prompt("choise Rock Paper Scissors");
-    if (user === null)
-        return 0;
-
-    user = user.toLowerCase();
     if (user ==="rock")
     {
         console.log("User: Rock");
@@ -24,12 +19,6 @@ function user_Input()
         console.log("User: Scissors");
         user = 1;
     }
-    else
-    {
-        console.log(`User: ${user}`);
-        return 0;
-    }
-
     return user;
 }
 
@@ -52,47 +41,104 @@ function cpu_choice()
         console.log("CPU: Rock");
         cpu = 3;
     }
-
     return cpu;
 }
 
-
-function main()
+function final_result(re_final, msg)
 {
-    if (user_score === 5 || cpu_score === 5)
-    {
-        if (user_score === 5)
-            console.log("finally you Win😒");
-        else
-            console.log("you fail like it’s a talent🥶");
-        return ;
-    }
+    re_final.textContent = msg;
+}
 
+function result(re, msg)
+{
+    re.textContent = msg;
+}
+
+function score(sc, msg)
+{
+    sc.textContent = msg;
+}
+
+function the_game(user_in)
+{
     console.log(`🚩round: ${round}`);
-    console.log(`score: user(${user_score}) | cpu(${cpu_score})`);
     round++;
 
-    let user = user_Input();
-    if (user === 0)
-    {
-        console.log("please enter a correct Input");
-        main();
-    }
+    let user = user_Input(user_in);
     let cpu = cpu_choice();
-
+    
+    let re = document.querySelector(".result");
+    let final_re = document.querySelector(".final_result");
+    let sc = document.querySelector(".score");
+    let msg1;
+    let msg2;
     if (user == cpu)
-        console.log("draw, no point😶‍🌫️");
+    {
+        msg1 = "draw, no point😶‍🌫️";
+        msg2 = `score: user(${user_score}) | cpu(${cpu_score})`;
+        console.log(msg1);
+        console.log(msg2);
+        score(sc, msg2);
+        result(re, msg1);
+    }
     else if (user > cpu)
     {
-        console.log("Win, one point😉");
         user_score++;
+        console.log(`score: user(${user_score}) | cpu(${cpu_score})`);
+        console.log("Win, one point😉");
+        msg1 = "Win, one point😉";
+        msg2 = `score: user(${user_score}) | cpu(${cpu_score})`;
+        console.log(msg1);
+        console.log(msg2);
+        score(sc, msg2);
+        result(re, msg1);
     }
     else
     {
-        console.log("lose, breaking new records in failure every time🤡");
         cpu_score++;
+        console.log(`score: user(${user_score}) | cpu(${cpu_score})`);
+        console.log("lose, breaking new records in failure every time🤡"); 
+        msg1 = "lose, breaking new records in failure every time🤡";
+        msg2 = `score: user(${user_score}) | cpu(${cpu_score})`;
+        console.log(msg1);
+        console.log(msg2);
+        score(sc, msg2);
+        result(re, msg1);
     }
-    main();
+
+
+    if (user_score !== 5 && cpu_score !== 5)
+        console.log("--------------------------");
+    if (user_score === 5 || cpu_score === 5)
+    {
+        if (user_score === 5)
+        {
+            console.log("finally you Win😒");
+            msg1 = "finally you Win😒";
+        }
+        else
+        {
+            console.log("you fail like it’s a talent🥶");
+            msg1 = "you fail like it’s a talent🥶";
+        }
+        console.log("--------------------------");
+        final_result(final_re, msg1);
+        user_score = 0;
+        cpu_score = 0;
+        round = 0;
+        return ;
+    }
+}
+        
+function main()
+{
+    let rock_btn = document.querySelector(".Rock");
+    let paper_btn = document.querySelector(".Paper");
+    let scissors_btn = document.querySelector(".Scissors");
+
+    rock_btn.addEventListener("click", () => the_game("rock"));
+    paper_btn.addEventListener("click", () => the_game("paper"));
+    scissors_btn.addEventListener("click", () => the_game("scissors"));
 }
 
 let answer = main();
